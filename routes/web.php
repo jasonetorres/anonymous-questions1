2b3ed7661abd08786010a607fd29a5f4fb827b76
jasonetorres <jasontorres585@icloud.com>
2024-11-14 05:12:12 -0500

    redo route

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::post('/questions', [QuestionController::class, 'submit'])->name('submit.question');
+Route::post('/questions', [QuestionController::class, 'store'])->name('submit.question');
 
 Route::get('/admin', [QuestionController::class, 'index'])->name('admin.questions');
 Route::delete('/admin/questions/{id}', [QuestionController::class, 'destroy'])->name('delete.question');
 Route::delete('/questions/{id}', [QuestionController::class, 'destroy']);
+Route::get('/questions/create', [QuestionController::class, 'create']);
